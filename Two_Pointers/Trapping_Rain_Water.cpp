4eaab9ea5d5edfd798de217fd7a5c20098a8575b
ent0n29 <[stanoantonio29@gmail.com]>
2023-10-04 22:40:36 +0200

    added trapping rainwater solution

--- Two_Pointers/Trapping_Rain_Water.cpp
+++ Two_Pointers/Trapping_Rain_Water.cpp
@@ -6,21 +6,21 @@ public:
         if (height.empty())
             return 0;
         
-        int l = 0;
-        int r = height.size() - 1;
-        int leftMax = height[l];
-        int rightMax = height[r];
+        int left = 0;
+        int right = height.size() - 1;
+        int leftMax = height[left];
+        int rightMax = height[right];
         int water = 0;
         
-        while (l < r) {
+        while (left < right) {
             if (leftMax < rightMax) {
-                l+=1;
-                leftMax = max(leftMax, height[l]);
-                water += leftMax - height[l];
+                left+=1;
+                leftMax = max(leftMax, height[left]);
+                water += leftMax - height[left];
             } else {
-                r-=1;
-                rightMax = max(rightMax, height[r]);
-                water += rightMax - height[r];
+                right-=1;
+                rightMax = max(rightMax, height[right]);
+                water += rightMax - height[right];
             }
         }
         return water;
